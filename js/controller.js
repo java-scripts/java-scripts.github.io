e8d2978e86f3a04085198b942e519363d5af36bb
javascripts <saratsivam@gmail.com>
2015-08-23 21:00:41 +0530

    updated

--- js/controller.js
+++ js/controller.js
@@ -3,14 +3,18 @@
 angular.module('myapp',[])
 .controller('MainController',['$scope','$http',function(s,http){
 	s.data=[];http.get('js/data.json').success(function(data){s.data = data;});	
+	s.lazygallery=[];
 }]); 
 
-function loadContent(){
-	console.log(arguments);
+function loadContent(el,ll,ul){
+	var s = angular.element($(el)[0]).scope();	
+	s.lazygallery =s.lazygallery.concat(s.data.gallery2.slice(ll,ul));
+	s.$apply();	
 }
 
 var options = [
-    {selector: '#gallery2', offset: 100, callback: 'loadContent("#gallery2")'},    
+    {selector: '#gallery2', offset: 300, callback: 'loadContent("#gallery2",0,4)'},    
+	 {selector: '#gallery2', offset: 600, callback: 'loadContent("#gallery2",4,9)'},    
   ];
 Materialize.scrollFire(options);
   
