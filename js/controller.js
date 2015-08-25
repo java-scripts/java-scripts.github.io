73219a9a6bc8d7bf345d39baa32b3dd324e77797
Sarat Kumar K <saratsivam@gmail.com>
2015-08-25 15:57:45 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,24 +1,43 @@
  $(".button-collapse").sideNav();
-
 angular.module('myapp',[])
 .controller('MainController',['$scope','$http',function(s,http){
-	s.data=[];http.get('js/data.json').success(function(data){s.data = data;});	
-	s.lazygallery=[];
-}]); 
+	s.dm={};http.get('js/data.json').success(function(res){s.dm = res;});	
+	s.start=0;s.end=4;	
+	
+	s.filterData={};var filterData={};	
+	s.updateFilterData = function(fname){
+		filterData[fname]?delete filterData[fname]:filterData[fname]=true;				
+		s.filterData=filterData;
+	}
+	
+	
+}])
+.filter('cardfilter',function(){
 
-function loadContent(el,ll,ul){
-	var s = angular.element($(el)[0]).scope();	
-	s.lazygallery =s.lazygallery.concat(s.data.gallery2.slice(ll,ul));
-	s.$apply();	
+	var findMatch=function(r,filters){	
+		for(var key in filters){
+			if(r.labels.indexOf(key)!=-1)return true;
+		}
+		
+		return false;	
+	}	
+
+	return function(data, filters){		
+		if(!filters || angular.equals({}, filters)){return data;}
+		var result=[];		
+		data.forEach(function(r){
+			if(findMatch(r,filters))result.push(r);
+		});
+		return result;
+	};
+	
+});
+function loadContent(el,ll,ul){	
+	var s = angular.element($(el)[0]).scope();s.end+=4;s.$apply();	
 }
+var options=[];
+for(var i=0;i<5;i++){options.push({selector: '#scrollcontainer', offset: i*350+1400, callback: 'loadContent("#scrollcontainer")'})}
 
-var options = [
-    {selector: '#gallery2', offset: 200, callback: 'loadContent("#gallery2",0,4)'},    
-	 {selector: '#gallery2', offset: 500, callback: 'loadContent("#gallery2",4,8)'},    
-	 {selector: '#gallery2', offset: 800, callback: 'loadContent("#gallery2",8,12)'},    
-	 {selector: '#gallery2', offset: 1100, callback: 'loadContent("#gallery2",12,16)'},    
-	 {selector: '#gallery2', offset: 1100, callback: 'loadContent("#gallery2",16,18)'},    
-  ];
 Materialize.scrollFire(options);
   
   
