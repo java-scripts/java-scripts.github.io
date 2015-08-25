583c418a98f6681ccac005e876231c44386c1114
javascripts <saratsivam@gmail.com>
2015-08-25 19:32:52 +0530

    updated

--- js/controller.js
+++ js/controller.js
@@ -2,26 +2,20 @@
 angular.module('myapp',[])
 .controller('MainController',['$scope','$http',function(s,http){
 	s.dm={};http.get('js/data.json').success(function(res){s.dm = res;});	
-	s.start=0;s.end=4;	
-	
+	s.start=0;s.end=4;		
 	s.filterData={};var filterData={};	
 	s.updateFilterData = function(fname){
 		filterData[fname]?delete filterData[fname]:filterData[fname]=true;				
 		s.filterData=filterData;
-	}
-	
-	
+	};	
 }])
 .filter('cardfilter',function(){
-
 	var findMatch=function(r,filters){	
 		for(var key in filters){
 			if(r.labels.indexOf(key)!=-1)return true;
-		}
-		
+		}		
 		return false;	
-	}	
-
+	}
 	return function(data, filters){		
 		if(!filters || angular.equals({}, filters)){return data;}
 		var result=[];		
@@ -29,17 +23,11 @@ angular.module('myapp',[])
 			if(findMatch(r,filters))result.push(r);
 		});
 		return result;
-	};
-	
+	};	
 });
 function loadContent(el,ll,ul){	
 	var s = angular.element($(el)[0]).scope();s.end+=4;s.$apply();	
 }
 var options=[];
 for(var i=0;i<5;i++){options.push({selector: '#scrollcontainer', offset: i*350+1400, callback: 'loadContent("#scrollcontainer")'})}
-
 Materialize.scrollFire(options);
-  
-  
-  
-  
